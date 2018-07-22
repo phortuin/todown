14aa9ec1e7bba25f766992a7dfe16fbd5fe60b08
Anne Fortuin <phortuin@gmail.com>
2018-07-22 14:02:44 +0200

    feat: improve task & page list parameters

--- app/routes/pages/list.js
+++ app/routes/pages/list.js
@@ -2,7 +2,7 @@ const Page = require('@models/page')
 const renderer = require('@lib/renderer');
 
 module.exports = (req, res, next) => {
-	Page.find().sort({ content: 1 }).exec()
+	Page.find().sort({ is_sticky: -1, content: 1 }).exec()
 		.then(pages => res.send(renderer.render('views/pages.html', { pages })))
 		.catch(next)
 }
