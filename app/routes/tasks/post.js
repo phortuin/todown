387894253ccbfbb1f13614f1328c2832df26f3fe
Anne Fortuin <phortuin@gmail.com>
2018-08-07 16:34:53 +0200

    feat: sensible defaults when posting task

--- app/routes/tasks/post.js
+++ app/routes/tasks/post.js
@@ -3,6 +3,8 @@ const Task = require('@models/task')
 module.exports = (req, res, next) => {
 	const task = new Task()
 	task.content = req.body.content
+	task.is_done = false
+	task.is_actionable = false
 	task.save()
 		.then(task => res.redirect(`tasks/${task.id}`))
 		.catch(next)
