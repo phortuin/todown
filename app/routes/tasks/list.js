fba51ded59c4d3b554cdcd5cb0627def07bb886f
Anne Fortuin <phortuin@gmail.com>
2018-08-07 15:35:25 +0200

    feat: add search

--- app/routes/tasks/list.js
+++ app/routes/tasks/list.js
@@ -1,8 +1,13 @@
 const Task = require('@models/task')
 const renderer = require('@lib/renderer');
 
+function getFilter(query) {
+	return query !== undefined ? { content: { $regex: query } } : { is_done: {$ne: true} }
+}
+
 module.exports = (req, res, next) => {
-	Task.find({ is_done: {$ne: true} }).sort({ is_actionable: -1, content: 1 }).exec()
-		.then(tasks => res.send(renderer.render('views/tasks.html', { tasks })))
+	const query = req.query.q
+	Task.find(getFilter(query)).sort({ is_today: 1, is_actionable: -1, is_done: -1, content: 1 }).exec()
+		.then(tasks => res.send(renderer.render('views/tasks.html', { tasks, query })))
 		.catch(next)
 }
