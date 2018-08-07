7dcbeac22d7011606a94e42da1c30647f5f2853f
Anne Fortuin <phortuin@gmail.com>
2018-08-07 16:35:31 +0200

    fix: case insensitive search & improved sorting

--- app/routes/tasks/list.js
+++ app/routes/tasks/list.js
@@ -2,12 +2,12 @@ const Task = require('@models/task')
 const renderer = require('@lib/renderer');
 
 function getFilter(query) {
-	return query !== undefined ? { content: { $regex: query } } : { is_done: {$ne: true} }
+	return query !== undefined ? { content: { $regex: query, $options: 'i' } } : { is_done: {$ne: true} }
 }
 
 module.exports = (req, res, next) => {
 	const query = req.query.q
-	Task.find(getFilter(query)).sort({ is_today: 1, is_actionable: -1, is_done: -1, content: 1 }).exec()
+	Task.find(getFilter(query)).sort({ scheduled_date: -1, is_done: 1, is_actionable: -1, content: 1 }).exec() // `content` sometimes has a # as starting character, not represented in the task’s title, but title is virtual and cannot be sorted with
 		.then(tasks => res.send(renderer.render('views/tasks.html', { tasks, query })))
 		.catch(next)
 }
