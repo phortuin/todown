50ac42867261ee3bdff95d4c920ad6b55d8be8eb
Anne Fortuin <phortuin@gmail.com>
2018-08-08 17:30:16 +0200

    fix: redirect after posting no longer results in offline fallback page

--- src/service-worker.js
+++ src/service-worker.js
@@ -24,25 +24,30 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
 	const path = event.request.url.replace(`${self.location.origin}/`, '')
-	if (event.request.url.startsWith(self.location.origin)) {
+	if (event.request.method === 'GET' && event.request.url.startsWith(self.location.origin)) {
+		// Strangeness due to redirect after posting data
+		// This was copied from a post by @adactio
+		// https://medium.com/@adactio/handling-redirects-with-a-service-worker-514a310863cf
+		let request = new Request(event.request.url, {
+	        method: 'GET',
+	        headers: event.request.headers,
+	        mode: event.request.mode == 'navigate' ? 'cors' : event.request.mode,
+	        credentials: event.request.credentials,
+	        redirect: event.request.redirect
+	    })
 		event.respondWith(
-			fetch(event.request)
+			fetch(request)
 				.then(response => {
 					// Network is available, put response in cache if it's not a precached asset
 					if (PRECACHE_URIS.includes(path)) {
 						return response
 					} else {
 						return caches.open(RUNTIME)
-							.then(cache => cache.put(event.request, response.clone()).then(() => response))
+							.then(cache => cache.put(request, response.clone()).then(() => response))
 					}
 				})
-				.catch(() => caches.match(event.request))
-				.then(response => {
-					if (!response) {
-						return caches.match('assets/offline.html')
-					}
-					return response
-				})
+				.catch(() => caches.match(request))
+				.then(response => response || caches.match('assets/offline.html'))
 		)
 	}
 })
