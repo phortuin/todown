bbdec21f7ee268506fd827181643b607b01bf7e4
Anne Fortuin <phortuin@gmail.com>
2018-08-09 16:51:58 +0200

    feat: review shows only items with older or missing scheduled date

--- app/routes/review/get.js
+++ app/routes/review/get.js
@@ -1,8 +1,16 @@
 const Task = require('@models/task')
-const renderer = require('@lib/renderer');
+const renderer = require('@lib/renderer')
+const moment = require('moment')
 
 module.exports = (req, res, next) => {
-	Task.find({ is_done: {$ne: true}, is_actionable: true }).sort({ content: 1 }).exec()
+	Task.find({
+		is_done: { $ne: true },
+		is_actionable: true,
+		$or: [
+			{ scheduled_date: null },
+			{ scheduled_date: { $lt: moment().startOf('day') } },
+		]
+	}).sort({ content: 1 }).exec()
 		.then(tasks => res.send(renderer.render('views/review.html', { tasks })))
 		.catch(next)
 }
