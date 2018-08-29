e045cedfb9000d2347a7c36b914c5d75265a6f35
Anne Fortuin <phortuin@gmail.com>
2018-08-29 13:20:09 +0200

    feat: add task list to page + refactor task UI components + db actions

--- app/routes/pages/get.js
+++ app/routes/pages/get.js
@@ -1,8 +1,23 @@
 const Page = require('@models/page')
+const Promise = require('bluebird');
 const renderer = require('@lib/renderer');
+const getTaskList = require('@routes/tasks/list').getTaskList
+
+function getTaggedTasks(page) {
+	if (page.tags) {
+		return new Promise((resolve, reject) => {
+			getTaskList({ $text: { $search: page.tags }, is_done: { $ne: true }})
+				.then(tasks => resolve({ page, tasks }))
+				.catch(reject)
+		})
+	} else {
+		return Promise.resolve({ page, tasks: [] })
+	}
+}
 
 module.exports = (req, res, next) => {
 	Page.findById(req.params.id).exec()
-		.then(page => res.send(renderer.render('views/page.html', { page })))
+		.then(getTaggedTasks)
+		.then(pagesAndTasks => res.send(renderer.render('views/page.html', pagesAndTasks)))
 		.catch(next)
 }
--- app/routes/tasks/list.js
+++ app/routes/tasks/list.js
@@ -1,13 +1,20 @@
 const Task = require('@models/task')
 const renderer = require('@lib/renderer');
 
+module.exports = (req, res, next) => {
+	const query = req.query.q
+	getTaskList(getFilter(query))
+		.then(tasks => res.send(renderer.render('views/tasks.html', { tasks, query })))
+		.catch(next)
+}
+
+// Note on sorting: `content` sometimes has a # as starting character, not represented in the task’s title, but title is virtual and cannot be sorted with
+function getTaskList(filter) {
+	return Task.find(filter).sort({ scheduled_date: -1, is_done: 1, is_actionable: -1, content: 1 }).exec()
+}
+
 function getFilter(query) {
 	return query !== undefined ? { content: { $regex: query, $options: 'i' } } : { is_done: {$ne: true} }
 }
 
-module.exports = (req, res, next) => {
-	const query = req.query.q
-	Task.find(getFilter(query)).sort({ scheduled_date: -1, is_done: 1, is_actionable: -1, content: 1 }).exec() // `content` sometimes has a # as starting character, not represented in the task’s title, but title is virtual and cannot be sorted with
-		.then(tasks => res.send(renderer.render('views/tasks.html', { tasks, query })))
-		.catch(next)
-}
+module.exports.getTaskList = getTaskList
